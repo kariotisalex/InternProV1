0abed0f6c16cb76c5360130698782ac22d69a952
kariotisalex <kariotis.alexander@gmail.com>
2023-06-09 13:44:32 +0300

    completed ReadResult class 2nd commit

--- src/main/java/com/itsaur/internship/ReadResult.java
+++ src/main/java/com/itsaur/internship/ReadResult.java
@@ -1,4 +1,46 @@
 package com.itsaur.internship;
 
 public class ReadResult {
+    private int currentPosition;
+    private int usernameLength;
+    private int totalLength;
+    private int userPosition;
+    User userReadResult;
+
+    public int getUserPosition() {
+        return userPosition;
+    }
+
+    public void setUserPosition(int userPosition) {
+        this.userPosition = userPosition;
+    }
+
+    public ReadResult() {
+        userReadResult = new User();
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public int getUsernameLength() {
+        return usernameLength;
+    }
+
+    public void setUsernameLength(int usernameLength) {
+        this.usernameLength = usernameLength;
+    }
+
+    public int getTotalLength() {
+        return totalLength;
+    }
+
+    public void setTotalLength(int totalLength) {
+        this.totalLength = totalLength;
+    }
+
 }
