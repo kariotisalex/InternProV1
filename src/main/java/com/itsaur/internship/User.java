6a68d848fabd2894ad82cded720481b19e46232e
kariotisalex <kariotis.alexander@gmail.com>
2023-06-09 13:30:41 +0300

    completed user class

--- src/main/java/com/itsaur/internship/User.java
+++ src/main/java/com/itsaur/internship/User.java
@@ -1,7 +1,39 @@
 package com.itsaur.internship;
 
-public record User(String username, String password) {
-    public boolean matches(String otherPassword) {
-        return password.equals(otherPassword);
+public class User {
+        private String username;
+        private String password;
+
+        public User() {
+        }
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public boolean isUsernameEqual(String username) {
+            return this.username.equals(username);
+        }
+
+        public boolean isPasswordEqual(String password) {
+            return (this.password.equals(password));
+        }
     }
-}
+
