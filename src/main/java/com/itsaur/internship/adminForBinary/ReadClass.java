4a549afbd1e138921c75ceb90e0a6c5edfd9e35c
kariotisalex <kariotis.alexander@gmail.com>
2023-06-09 15:40:11 +0300

    adminClass reform

---
+++ src/main/java/com/itsaur/internship/adminForBinary/ReadClass.java
@@ -0,0 +1,97 @@
+package com.itsaur.internship.adminForBinary;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+
+public class ReadClass {
+
+    public static class ReadResult {
+        public int currentPosition;
+        public int usernameLength;
+        public int totalLength;
+        public String username;
+        public String password;
+
+        public boolean isEqual(String usernm, String passwd) {
+            return (password.equals(passwd)) && (username.equals(usernm));
+        }
+    }
+    private Vertx vertx;
+    public ReadClass(Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    public Future<ReadResult> showAll(){
+        return vertx
+                .fileSystem()
+                .open( "/home/kariotis@ad.itsaur.com/IdeaProjects/RevisionV1/src/main/java/RestAPI/users.bin",//"/home/kariotis@ad.itsaur.com/Downloads/u2.bin",
+                        new OpenOptions())
+                .compose(file ->{
+                    return readNextUser(file, 0,null, null, false);
+                });
+
+    }
+
+
+    public Future<ReadResult> showAllResult(String path){
+        return vertx
+                .fileSystem()
+                .open( path,
+                        new OpenOptions())
+                .compose(file ->{
+                    return readNextUser(file, 0,null, null, false);
+                });
+    }
+    private Future<ReadResult> readNextUser(AsyncFile file, final int currentPosition, String usernm, String passwd, boolean aTest) {
+        return file
+                .read(Buffer.buffer(), 0, currentPosition, 2)
+                .map(totalSizeBuf -> {
+                    ReadResult readResult = new ReadResult();
+
+                    readResult.currentPosition = currentPosition + 2;
+                    readResult.totalLength = totalSizeBuf.getBytes()[0];
+                    readResult.usernameLength = totalSizeBuf.getBytes()[1];
+
+                    return readResult;
+                })
+                .compose(readResult -> file
+                        .read(Buffer.buffer(), 0, readResult.currentPosition, readResult.usernameLength)
+                        .map(usernameBuf -> {
+                            readResult.currentPosition = readResult.currentPosition + readResult.usernameLength;
+                            readResult.username = new String(usernameBuf.getBytes());
+                            return readResult;
+                }))
+                .compose(readResult -> file
+                        .read(Buffer.buffer(), 0, readResult.currentPosition, readResult.totalLength - (2 + readResult.usernameLength))
+                        .map(passwordBuf -> {
+                            readResult.currentPosition = readResult.currentPosition + (readResult.totalLength - (2 + readResult.usernameLength));
+                            readResult.password = new String(passwordBuf.getBytes());
+                            return readResult;
+                }))
+                .compose(readResult -> {
+
+                    if (readResult.isEqual(usernm, passwd) && aTest ){
+                        readResult.currentPosition = (int) file.sizeBlocking() + 1;
+                        System.out.print("The user : " +
+                                "" + readResult.username + " with password : " + readResult.password);
+                    }else if (!aTest){
+                        System.out.println(readResult.username + " " + readResult.password);
+                    }
+
+                    if (readResult.currentPosition == file.sizeBlocking()) {
+                        return Future.failedFuture(new IllegalArgumentException());
+                    } else if (readResult.currentPosition == file.sizeBlocking() + 1) {
+                        return Future.succeededFuture();
+                    } else {
+                        return readNextUser(file, readResult.currentPosition, usernm, passwd, aTest);
+                    }
+                });
+    }
+
+
+
+
+}
---
+++ src/main/java/com/itsaur/internship/adminForBinary/RestAPI.java
@@ -0,0 +1,34 @@
+package com.itsaur.internship.adminForBinary;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
+public class RestAPI {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        Router router = Router.router(vertx);
+        HttpServer server = vertx.createHttpServer();
+
+        ReadClass readClass = new ReadClass(vertx);
+
+        router
+                .get("/showAll")
+                .handler(v -> {
+                    readClass.showAll();
+                    v.end();
+                });
+
+        router
+                .get("/showAllResult")
+                .handler(v -> {
+                    readClass.showAllResult( "/home/kariotis@ad.itsaur.com/IdeaProjects/RevisionV1/src/main/java/RestAPI/users22.bin");
+                    v.end();
+                });
+
+        server.requestHandler(router).listen(8081);
+    }
+}
