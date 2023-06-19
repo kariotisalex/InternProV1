ecccc43770f32e3bb4cfc3b2cb3c1b1fba3390d5
kariotisalex <kariotis.alexander@gmail.com>
2023-06-19 14:20:14 +0300

    Fine tuning

--- src/main/java/com/itsaur/internship/adminService/postgreSQL/CreateUserInPostgreSQL.java
+++ src/main/java/com/itsaur/internship/adminService/postgreSQL/CreateUserInPostgreSQL.java
@@ -1,5 +1,6 @@
 package com.itsaur.internship.adminService.postgreSQL;
 
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
@@ -24,98 +25,65 @@ public class CreateUserInPostgreSQL {
             .setMaxSize(5);
 
     public static void main(String[] args) {
+        args = new String[]{"a"};
 
-        //new CreateUserInPostgreSQL().generateRecords(10);
-        new CreateUserInPostgreSQL().addUsers(10);
+        new CreateUserInPostgreSQL().generateRecords(10);
+        //new CreateUserInPostgreSQL().addUsers(10);
 
     }
-
-
-    public void addUsers(int records){
+    public Future<Void> addUsers(int records){
         SqlClient client = PgPool.client(vertx, connectOptions, poolOptions);
 
-        client
+        return client
                 .query("SELECT EXISTS( SELECT FROM users)")
                 .execute()
-                .onComplete(res -> {
-                    if (res.succeeded()){
-                                        insertRandomUsers(client, 0, records);
-                    }
-
+                .compose(res -> {
+                    return insertRandomUsers(client, records);
                 });
     }
+
     public void generateRecords(int records){
         SqlClient client = PgPool.client(vertx, connectOptions, poolOptions);
 
         client
                 .query("SELECT EXISTS( SELECT FROM users)")
                 .execute()
-
-                .onComplete(res ->
-                { if (res.succeeded()){
-                    RowSet<Row> rows = res.result();
-                    for (Row row:rows)
-                        System.out.println(row.getBoolean(0));
-
-
-
+                .onComplete(res ->{
+                    if (res.succeeded()){
+                        RowSet<Row> rows = res.result();
+                        System.out.println(rows.iterator().next().getBoolean(0));
                     client.close();
                 }else {
-                    generateUsers(0, records);
+                    generateUsers(records);
                 }
-
-
                 });
-
     }
-    private void generateUsers(int start, int records){
-
+    private Future<Void> generateUsers(int records){
         SqlClient client = PgPool.client(vertx, connectOptions, poolOptions);
 
-        client
-
+        return client
                 .query("CREATE TABLE users (\n" +
                         "    personid uuid primary key not null,\n" +
                         "    username varchar(255),\n" +
                         "    password varchar(255)\n" +
                         "); ")
-                        .execute()
-                                .onComplete(eq -> {
-                                    if(eq.succeeded()){
-                                        insertRandomUsers(client,start,records);
-                                        client.close();
-                                    }else {
-                                        System.out.println(eq.cause());
-                                    }
-
-
-                                });
-
-
-
-
-
-
-    }
-
-    private static void insertRandomUsers(SqlClient client, int startPosition, int records){
-        client
-                .preparedQuery("INSERT INTO users (personid, username,password) VALUES ($1, $2, $3)")
-                .executeBatch(tupleFiller(startPosition, records))
-                .onComplete(qwe -> {
-                    if (qwe.succeeded()){
-                        RowSet<Row> rows = qwe.result();
-
-
-                    }else {
-                        System.out.println("Batch failed " + qwe.cause());
-                    }
+                .execute()
+                .compose(eq -> {
+                    addUsers(records);
+                    return client.close();
                 });
     }
-    private static List<Tuple> tupleFiller(int start, int records){
-        records += start;
+
+    private static Future<Void> insertRandomUsers(SqlClient client, int records){
+        return client
+                .preparedQuery("INSERT INTO users (personid, username,password) VALUES ($1, $2, $3)")
+                .executeBatch(tupleFiller(records))
+                .mapEmpty();
+    }
+    private static List<Tuple> tupleFiller(int records){
+
         List<Tuple> batch = new ArrayList<>();
-        IntStream.range(start,records).forEach(e ->{
+        IntStream.range(0, records).forEach(e ->{
             batch.add(Tuple.of(UUID.randomUUID(), generateRandom(1), generateRandom(0)));
         });
         return batch;
